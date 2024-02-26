7aa926daf4bc27356764a2848913deacc59968a6
Aria <montaghed.man@gmail.com>
2024-02-26 05:35:09 +0330

    ThirdCommit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 let icon = document.getElementById("js-func");
 let div1 = document.getElementById("js-func2");
+let service = document.querySelectorAll("#service");
+let counter = 0;
 
 icon.addEventListener("click", () => {
   icon.classList.toggle("bi-list");
@@ -7,3 +9,19 @@ icon.addEventListener("click", () => {
   div1.classList.toggle("hidden");
   div1.classList.toggle("flex");
 });
+
+const myFunc = () => {
+  if (counter === 0) {
+    service[0].classList.remove("opacity-50");
+    service[6].classList.add("opacity-50");
+  } else {
+    service[counter - 1].classList.add("opacity-50");
+    service[counter].classList.remove("opacity-50");
+  }
+  counter += 1;
+  if (counter === 7) {
+    counter = 0;
+  }
+};
+
+setInterval(myFunc, 5000);
