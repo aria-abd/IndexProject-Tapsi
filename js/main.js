c67887df83a5ba6ed8c5f34cd7abf21bb4bf7ba6
Aria <montaghed.man@gmail.com>
2024-02-28 02:59:17 +0330

    FourthCommit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 let icon = document.getElementById("js-func");
 let div1 = document.getElementById("js-func2");
-let service = document.querySelectorAll("#service");
+let service = document.querySelectorAll(".service");
+let component = document.querySelectorAll(".component");
+let divSelector = document.querySelectorAll(".divSelector");
 let counter = 0;
 
 icon.addEventListener("click", () => {
@@ -14,14 +16,40 @@ const myFunc = () => {
   if (counter === 0) {
     service[0].classList.remove("opacity-50");
     service[6].classList.add("opacity-50");
+    // /////////////
+    component[0].classList.remove("hidden"); // asli
+    component[0].classList.add("flex");
+    component[6].classList.add("hidden"); // asli
+    component[6].classList.remove("flex");
   } else {
     service[counter - 1].classList.add("opacity-50");
     service[counter].classList.remove("opacity-50");
+    // /////////////
+    component[counter - 1].classList.add("hidden"); // asli
+    component[counter - 1].classList.remove("flex");
+    component[counter].classList.remove("hidden"); // asli
+    component[counter].classList.add("flex");
   }
   counter += 1;
   if (counter === 7) {
     counter = 0;
+    clearInterval(intervalId);
   }
 };
 
-setInterval(myFunc, 5000);
+const intervalId = setInterval(myFunc, 3000);
+
+// divSelector[counter].addEventListener("click", () => {});
+
+for (let i = 0; i < divSelector.length; i++) {
+  // text += cars[i] + "<br>";
+  console.log("hi");
+  divSelector[i].classList.add(0 + i);
+  divSelector[i].addEventListener("click", (e) => {
+    console.log(e);
+  });
+}
+
+// divSelector[0].addEventListener("click", () => {
+//   console.log(`hi1`);
+// });
